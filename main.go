3b60c7efd19af0841b5cc5c61c01a998c4e07a4b
Aliaksei Maniuk <aliaksei.maniuk@gmail.com>
2018-02-28 20:11:56 -0500

    Caching for api call.

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
+	cache "github.com/patrickmn/go-cache"
 )
+
+var c = cache.New(30*time.Minute, 60*time.Minute)
 
 func main() {
 	router := mux.NewRouter()
@@ -16,13 +20,22 @@ func main() {
 }
 
 func getOpenDataAirports(w http.ResponseWriter, r *http.Request) {
-	var airportUrl = "https://data.delaware.gov/api/views/mh8v-eba6/rows.json?accessType=DOWNLOAD"
-	resp, err := http.Get(airportUrl)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	var airportURL = "https://data.delaware.gov/api/views/mh8v-eba6/rows.json?accessType=DOWNLOAD"
+	var airportJsonKey = "airportJson"
 
-	body, err := ioutil.ReadAll(resp.Body)
-	w.Write(body)
+	airportJsonData, found := c.Get(airportJsonKey)
+	if found {
+		json := airportJsonData.(string)
+		w.Write([]byte(json))
+	} else {
+		resp, err := http.Get(airportURL)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		c.Set(airportJsonKey, string(body[:]), cache.DefaultExpiration)
+		w.Write(body)
+	}
 }
